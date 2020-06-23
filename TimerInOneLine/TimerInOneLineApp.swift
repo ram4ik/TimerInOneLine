229b0db580b3e4c59386477739173d83653ffb8a
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-06-23 08:10:01 +0300

    Initial Commit

---
+++ TimerInOneLine/TimerInOneLineApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerInOneLineApp.swift
+//  TimerInOneLine
+//
+//  Created by Ramill Ibragimov on 23.06.2020.
+//
+
+import SwiftUI
+
+@main
+struct TimerInOneLineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
